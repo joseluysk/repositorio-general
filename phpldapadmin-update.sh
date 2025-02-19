bd6fc2ccc2f89f9e6f0cb2835150440c728b218f
joseluysk <jconang499@g.educaand.es>
2025-02-19 08:51:44 +0000

    Subido el 19 feb 2025

---
+++ phpldapadmin-update.sh
@@ -0,0 +1,4 @@
+#Script que actualiza PHP Ldap Admin a una versión compatible con PHP8.1 en Ubuntu Server 22.04
+#En versiones posteriores del server está solucionado
+
+sudo dpkg -i phpldapadmin_1.2.6.7-2_all.deb
